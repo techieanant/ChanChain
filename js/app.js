ad77d933eb5c25e4bf41bdb6b27c22366ab747cb
Anant <techieanant@gmail.com>
2018-08-15 22:42:35 -0400

    Some more JS;

--- js/app.js
+++ js/app.js
@@ -1,44 +1,115 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: 0x0,
+  loading: false,
 
-$(window).on("ready load change", function(){
-  setInterval(detectWeb3,1000);
+  init: function() {
+    return App.initWeb3();
+  },
 
-});
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:9545');
+      web3 = new Web3(App.web3Provider);
+    }
 
+    if(web3.eth.accounts.length > 0) {
+      $("#alertRow").addClass("d-none");
+    } else {
+      $("#alertRow").removeClass("d-none");
+    }
 
+    App.displayAccountInfo();
+    return App.initContract();
+  },
 
-const contract = $.getJSON( "./build/contracts/ChanChain.json", function(data) {
-  return data;
-});
-
-console.log(contract);
-
-function detectWeb3(){
-  if (typeof web3 !== 'undefined' && web3.eth.accounts.length > 0) {
-    web3js = new Web3(web3.currentProvider);
-    $("#alertRow").addClass("d-none");
-    updateAccountDetails();
-  } else {
-    $("#alertRow").removeClass("d-none");  }
-}
-
-
-function updateAccountDetails() {
-  if(web3js.eth.defaultAccount) {
-    $("#wallet-address").text(web3js.eth.defaultAccount);
-    web3js.eth.getBalance(web3js.eth.defaultAccount, function(error, result){
-      if(result) {
-        $("#wallet-balance").text(web3.fromWei(result, "ether"));
-      } else {
-        $("#wallet-balance").text("Null");
-        console.log("Cannot get Balance; Check Metamask connection!")
+  displayAccountInfo: function() {
+  if(web3.eth.accounts.length > 0) {
+    App.account = web3.eth.coinbase;
+    $("#wallet-address").text(App.account);
+    web3.eth.getBalance(App.account, function(err, balance) {
+      if (err === null) {
+        $("#wallet-balance").text(web3.fromWei(balance, "ether").toFixed(2));
       }
-    })
+    });
   } else {
-    $("#wallet-balance").text("Null");
     $("#wallet-address").text("Null");
+    $("#wallet-balance").text("Null");
   }
-}
 
-$(".card").click(function(){
-  alert("AsdaSD");
+  },
+
+  initContract: function() {
+    $.getJSON('./build/contracts/ChanChain.json', function(artifact) {
+      // Get the necessary contract artifact file and use it to instantiate a truffle contract abstraction.
+      App.contracts.ChanChain = TruffleContract(artifact);
+
+      // Set the provider for our contract.
+      App.contracts.ChanChain.setProvider(App.web3Provider);
+
+      // Listen for events
+      // App.listenToEvents();
+      App.fetchAllThreads();
+      console.log(App.contracts.ChanChain.abi);
+      // Retrieve the article from the smart contract
+      // return App.reloadArticles();
+    });
+  },
+
+  fetchAllThreads: function(){
+    App.contracts.ChanChain.deployed().then(instance => {
+      instance.indexThreads.call().then(index => {
+        for(i=1; i <= index.toNumber() - 1; i++) {
+            instance.threads(i).then(data => {
+              App.displayThreadCards(data[0], data[1], data[4].toNumber());
+            });
+        }
+      });
+    });
+  },
+
+  displayThreadCards: function(text, imgsrc, timestamp) {
+    var cardsRow = $('#cardsRow');
+    var cardTemplate = $('#cardTemplate');
+    var shortText = jQuery.trim(text).substring(0, 30).split(" ").slice(0, -1).join(" ") + "...";
+    cardTemplate.find('.card-text').text(shortText);
+    cardTemplate.find('.card-img-top').attr('src', imgsrc);
+    cardTemplate.find('.card-header').text(timestamp);
+    cardTemplate.find('.card').removeClass('d-none');
+    cardsRow.append(cardTemplate.html());
+  },
+
+  // Listen for events raised from the contract
+  listenToEvents: function() {
+    App.contracts.ChanChain.deployed().then(function(instance) {
+      instance.sellArticleEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        $("#events").append('<li class="list-group-item">' + event.args
+          ._name + ' is for sale' + '</li>');
+        App.reloadArticles();
+      });
+
+      instance.buyArticleEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        $("#events").append('<li class="list-group-item">' + event.args
+          ._buyer + ' bought ' + event.args._name + '</li>');
+        App.reloadArticles();
+      });
+    });
+  },
+
+};
+
+$(function() {
+  $(window).ready(function() {
+    App.init();
+  });
 });
